17979b3ab7cce446c489836d2499a09ade33cb52
llelotte <lucie.lelotte@uliege.be>
2023-11-17 09:22:07 +0100

    my first commit

---
+++ github.R
@@ -0,0 +1,10 @@
+library(usethis)
+use_git_config(
+  user.name = "kingsleyshaklebolt", 
+  user.email = "kingsleyshaklebolt@ministryofmagic.com"
+)
+
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+usethis::git_vaccinate()
+usethis::use_git()
